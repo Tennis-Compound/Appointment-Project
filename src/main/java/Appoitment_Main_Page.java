0f6ab29a5b9e5962641a41a65c5659d691146534
karam <karam@karam>
2026-03-01 21:05:28 +0200

    US 1.1 BY Karam Khader

--- src/main/java/Appoitment_Main_Page.java
+++ src/main/java/Appoitment_Main_Page.java
@@ -2,28 +2,40 @@ import java.util.Scanner;
 public class Appoitment_Main_Page {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Appointment Scheduling System");
-		System.out.println("Enter the number following what you want to do");
+		
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("1- Administrator Login ");
-		
-		int chosenNum = input.nextInt();
-		input.nextLine();
+		int chosenNum = showMenu(input);
+
 		if(chosenNum == 1) {
-			System.out.println("Enter username: ");
-	        String username = input.nextLine();
-	        
-	        System.out.println("Enter password: ");
-	        String password = input.nextLine();
-	        
-	        String adminUsername = "admin1";
-	        String adminPassword = "12345";
-	        if(username.equals(adminUsername) && password.equals(adminPassword)) {
-	        	System.out.println("Login Successful");
-	        }else {
-	        	System.out.println("Invalid Credentials");
-	        }
+			adminLogin(input);
+			
 		}
 		input.close();
+	}
+	//Menu Function
+	private static int showMenu(Scanner input) {
+		System.out.println("Enter the number following what you want to do");
+		System.out.println("1- Administrator Login ");
+		
+		int choice = input.nextInt();
+		input.nextLine();
+		return choice;
+	}
+	
+	private static void adminLogin(Scanner input) {
+		System.out.println("Enter username: ");
+        String username = input.nextLine();
+        
+        System.out.println("Enter password: ");
+        String password = input.nextLine();
+        
+        String adminUsername = "admin1";
+        String adminPassword = "12345";
+        if(username.equals(adminUsername) && password.equals(adminPassword)) {
+        	System.out.println("Login Successful");
+        }else {
+        	System.out.println("Invalid Credentials");
+        }
 	}
 }
