44b183253fc0b607db1cc792b3dc5529f09edcf1
Karam Majdi Khader <146377267+karamkhader@users.noreply.github.com>
2026-05-08 00:00:03 +0300

    Implement sendReminder method in SimpleNotificationService

--- src/main/java/SimpleNotificationService.java
+++ src/main/java/SimpleNotificationService.java
@@ -12,6 +12,11 @@ public class SimpleNotificationService implements NotificationService {
         System.out.println("Cancellation notice sent to " + email + ": " + message);
     }
 
+    @Override
+    public void sendReminder(String email, String message) {
+        System.out.println("Reminder sent to " + email + ": " + message);
+    }
+
     public void sendModificationNotice(String email, String oldDetails, String newDetails) {
         System.out.println("Modification notice sent to " + email
             + " | Old: " + oldDetails + " | New: " + newDetails);
