dc9c20cd878881a08daa5ebfce9b425ac67b4cd5
3mr122 <amrfadda597@gmail.com>
2026-05-09 23:20:52 +0300

    Update BookingRulesCoverageTest.java

--- src/test/java/BookingRulesCoverageTest.java
+++ src/test/java/BookingRulesCoverageTest.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("Booking Rules Strategy Coverage")
 class BookingRulesCoverageTest {
